b91a618a90856a6c222565c1ef5e038cb2ea8def
Mexaff <70278192+Mexaff@users.noreply.github.com>
2020-09-02 19:37:50 +0300

    trid lection

--- ul.php
+++ ul.php
@@ -1,26 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-    <style>
-		.mark {
-			margin: 0;
-			padding: 4px;
-		}
-		.mark li{
-			display: inline;
-			margin-right: 10px;
-			border: 1px solid;
-			padding: 3px;
-		}
-	</style>
-		
+	<head>
+	</head>
 	
-	<ul class="mark">
-		<li>data1</li>
-		<li>data2</li>
-		<li>data3</li>
-		<li>data4</li>
-		<li>data5</li>
+	<body>
+		<style>
+			.mark {
+				margin: 0;
+				padding: 4px;
+			}
+			.mark li{
+				display: inline;
+				margin-right: 10px;
+				border: 1px solid;
+				padding: 3px;
+			}
+		</style>
+			
 		
-	</ul>
-
+		<ul class="mark">
+			<li>data1</li>
+			<li>data2</li>
+			<li>data3</li>
+			<li>data4</li>
+			<li>data5</li>
+			
+		</ul>
+	</body>
 </html>
