4cc4e50708b677bbf2a41094880ee658437996ea
Mexaff <70278192+Mexaff@users.noreply.github.com>
2020-09-09 18:57:57 +0300

    Lesson5

--- index.php
+++ index.php
@@ -1,56 +1,46 @@
 <?php
 
-$task1 = 7 % 3;
-$task2= intdiv((7+7.15),1);
-$task3 = sqrt(25);
+ $a = 5;
+ function f1($b)
+ {
+    var_export($b);
+     echo '<br>';
+ }
 
-$text = "Десять негритят пошли купаться в море";
+ f1($a);
 
-$task4 = substr("$text", 41, 16);
-$task5 = substr("$text", 30,2);
-//$task6 = ucwords($text);
-$task6 = mb_convert_case($text, MB_CASE_TITLE, "UTF-8");
-$task7 = mb_strlen($text);
+ function f2($b)
+ {
+     return $b + $b;
+ }
 
-$true = true;
-$false = false;
-$three = "three";
-$three_2 = "три";
+ echo f2($a).'<br>';
 
-echo "Задание 1:<br> $task1<br>";
-echo "Задание 2:<br> $task2<br>";
-echo "Задание 3:<br> $task3<br>";
-echo "Задание 4:<br> $task4<br>";
-echo "Задание 5:<br> $task5<br>";
-echo "Задание 6:<br> $task6<br>";
-echo "Задание 7:<br> $task7<br>";
+ function f3($var)
+ {
+     $var = 10;
+     $var = f2($var);
+     f1($var);
+ }
 
-echo "Задание 8:<br>";
-if($true == 1) {
-    echo "True равно 1<br>";
-} else {
-    echo "true не равно 1<br>";
+ f3(5);
+
+ function f4($var = 10)
+ {
+     $var = f2($var);
+     f1($var);
+ }
+ f4();
+ f4(7);
+
+function f5($var = NULL)
+{
+    if(NULL === $var) {
+        $var = 10;
+    }
+    $var = f2($var);
+    f1($var);
 }
 
-echo "Задание 9:<br>";
-if($false === 0) {
-    echo "false тождественно 0<br>";
-} else {
-    echo "false не дождественно 0<br>";
-}
-
-echo "Задание 10:<br>";
-if(mb_strlen($three) > mb_strlen($three_2)) {
-    echo "строка three длиннее<br>";
-} else {
-    echo "строка три длиннее<br>";
-}
-
-echo "Задание 11:<br>";
-if((125 * 13 + 7) > (223 + 28 * 2)) {
-    echo "первое число больше<br>";
-} else {
-    echo "второе число больше<br>";
-}
-
+f5();
 
