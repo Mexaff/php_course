f901072804728eff63582119faced84f3ca206a6
unknown <denis30042002@gmail.com>
2020-10-13 20:47:43 +0300

    Homework12_

--- core/Database/Insert.php
+++ core/Database/Insert.php
@@ -7,10 +7,18 @@ namespace Core\Database;
 class Insert
 {
     protected $link;
-    public function __constructor()
+    public function __construct()
     {
-        $dbconnecter = new Connecter;
-        $dbconnecter->connectDB();
+        //$dbconnecter = new Connecter();
+        $dbconnecter = new \mysqli('127.0.0.1', 'root', 'root', 'test');
+        $link = $dbconnecter->connectDB();
     }
+
+    public function insert($table, $column1 = null, $column2 = null, $column3 = null, $column4 = null, $column5 = null)
+    {
+        
+    }
+
+
 
 }
